80147ba6c9070aec59d0590340e56c7cc4c1375d
Dan Bartlett <dan@danbartlett.com>
2020-05-17 10:56:09 -0700

    Final ui tweaks

--- amplify-vue-admin-module/src/components/Header.vue
+++ amplify-vue-admin-module/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 
   <nav class="navbar  nav-pills navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">AWS Simple CMS Admin Module</a>
+    <a class="navbar-brand" href="#">Serverless CMS Admin</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
